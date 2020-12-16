185eac53cdaf75ed9cbd478c632467a520d91653
Lara <lenichols602@gmail.com>
2020-12-15 20:59:13 -0600

    (add) create index file Co-authored-by: Chris cjbeats@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render } from 'react-dom';
+import { ReactReduxContext } from "react-redux";
+import { Provider } from 'react-redux';
+import App from './containers/App.jsx';
+import store from './store';
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello World from React boilerplate</h1>;
-  }
-}
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello World from React boilerplate</h1>;
+//   }
+// }
+// ReactDOM.render(<Welcome />, document.getElementById("root"));
+
+render (
+  <Provider store = { store }>
+    <App />
+    <h2>TESTINGGGG</h2>
+  </Provider>,
+  document.getElementById('root')
+);
