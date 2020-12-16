68c93aa809f012a2ec6f9b1d72264108bcafbead
Lara <lenichols602@gmail.com>
2020-12-15 21:02:17 -0600

    (add) created store Co-authored-by: Chris cjbeats@gmail.com

---
+++ src/store.js
@@ -0,0 +1,24 @@
+
+/**
+ * ************************************
+ *
+ * @module  store.js
+ * @author
+ * @date
+ * @description Redux 'single source of truth'
+ *
+ * ************************************
+ */
+
+ import { createStore, applyMiddleware } from 'redux';
+ import { composeWithDevTools } from 'redux-devtools-extension';
+ import reducers from './reducers/index.js';
+ import ReduxThunk from 'redux-thunk';
+
+ const composedEnhancer = composeWithDevTools(applyMiddleware(ReduxThunk));
+
+ const store = createStore(
+   reducers, composedEnhancer
+ );
+
+ export default store; 
