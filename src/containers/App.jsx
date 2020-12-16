cf91e2f0de97a00f52127994bc11d5c546b3f128
Lara <lenichols602@gmail.com>
2020-12-15 21:01:24 -0600

    (add) created containers folder Co-authored-by: Chris cjbeats@gmail.com

---
+++ src/containers/App.jsx
@@ -0,0 +1,35 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author Lara, Chris, Carlos
+ * @date 12/15/2020
+ * @description renders React app
+ *
+ * ************************************
+ */
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import '../styles/styles.scss';
+
+
+
+// const mapDispatchToPreps = (dispatch) => ({
+
+// })
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>TESTING REACT APP</h2>
+    )
+  }
+}
+
+// export default connect(null, mapDispatchToProps)(App);
+export default App;
