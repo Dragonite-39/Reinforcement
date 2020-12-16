ea733cd58fbbd896032b309a4992d2a155d904fc
Lara <lenichols602@gmail.com>
2020-12-15 21:01:46 -0600

    (add) created reducers Co-authored-by: Chris cjbeats@gmail.com

---
+++ src/reducers/restaurantReducer.js
@@ -0,0 +1,40 @@
+/**
+ * ************************************
+ *
+ * @module  restaurantReducer
+ * @author Lara, Chris
+ * @date 12/16.
+ * @description reducer for restaurants
+ *
+ * ************************************
+ */
+
+ import * as types from '../constants/actionTypes.js';
+
+ const initialState = {
+   
+ };
+
+ const restaurantReducer = (state = initialState, action) => {
+
+
+    switch(action.type){
+
+      case types.FILTER_RESTAURANTS:
+
+
+      case types.ADD_RESTAURANT:
+
+
+      case types.EDIT_RESTAURANT:
+
+
+      case types.ADD_COMMENT:
+
+
+      case types.GET_COMMENTS:
+
+
+    
+    }
+ }
