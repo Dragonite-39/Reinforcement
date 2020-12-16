9b03d39e66ab806ed2cc60d7f6bbcebc2bb3d00b
Lara <lenichols602@gmail.com>
2020-12-16 12:54:40 -0600

    (update) edited initial state Co-authored-by: Chris cjbeats@gmail.com

--- src/reducers/restaurantReducer.js
+++ src/reducers/restaurantReducer.js
@@ -12,7 +12,44 @@
  import * as types from '../constants/actionTypes.js';
 
  const initialState = {
-   
+    currentRestaurant: null,
+    filterInfo: {
+      zip: null,
+      cuisine: [],
+      amenities: []
+    },
+    filteredRestaurants: [],
+      // Structure of an element on the filteredRestaurants array
+      // {
+      //   name: '',
+      //   address: '',
+      //   cuisine: '',
+      //   open: boolean
+      // }
+    currentRestaurantData: {},
+      // structure of currentRestaurantData obj
+      // {
+      //   name: '',
+      //   restaurantId: '',
+      //   cuisine: '',
+      //   address: '',
+      //   phone: '',
+      //   website: '',
+      //   amenities: [],
+      //   hours: Boolean,
+      //   menu: Boolean,
+      //   open: Boolean,
+      //   comments: [
+      //     {
+      //       commintId: '', 
+      //       text: '',
+      //       username: '',
+      //       timestamp: ''
+      //     }
+      //   ]
+      // }
+    username: '',
+    userId: ''
  };
 
  const restaurantReducer = (state = initialState, action) => {
@@ -35,6 +72,10 @@
       case types.GET_COMMENTS:
 
 
+      default:
+        return state;
     
     }
  }
+
+ export default restaurantReducer;
